ae06214ee28d41469e0056db070ebc250a35daac
ritram <riteshraman25@gmail.com>
2021-05-21 13:16:33 +0100

    first commit

---
+++ src/test/java/com/example/jenkins/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.jenkins.api;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case executing...");
+		Assertions.assertTrue(true);
+	}
+
+}
